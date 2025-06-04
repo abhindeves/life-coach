65193499e04e1d6ea3814a720d924fe1f60f8f37
abhindeves <abhindev.es@gmail.com>
2025-06-04 11:19:16 +0530

    feat: Initial commit - Basic Telegram bot with MongoDB integration

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+# --- MongoDB Configuration ---
+username = quote_plus(os.getenv("MONGO_USERNAME"))
+password = quote_plus(os.getenv("MONGO_PASSWORD"))
+uri = f"mongodb+srv://{username}:{password}@cluster0.xjcfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# ... rest of your code ...
+
+
+def main():
+    print("Hello from coach!")
+
+if __name__ == "__main__":
+    main()
